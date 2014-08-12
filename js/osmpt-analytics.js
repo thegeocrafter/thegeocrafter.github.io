ae441cbb1256cd6d75db36a39b1c48ee6cc1efde
thegeocrafter <geocrafter.the@gmail.com>
2014-08-12 11:48:30 +0100

    Analytics fix

--- js/osmpt-analytics.js
+++ js/osmpt-analytics.js
@@ -5,3 +5,12 @@
 
   ga('create', 'UA-52930082-2', 'auto');
   ga('send', 'pageview');
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-52930082-2']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
