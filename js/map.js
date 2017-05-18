67e0f91565bbce8cf71e3035473b00e54e168fd6
thegeocrafter <geocrafter.the@gmail.com>
2017-05-18 16:19:42 +0200

    added new material for civitate and changed residential address

--- js/map.js
+++ js/map.js
@@ -1,18 +1,20 @@
 
 $(function () {
-        var contactMap = new L.Map('map', {maxBounds: ([[40.0603,-8.5501],[40.1405,-8.3908]])}).setView([40.115, -8.5122], 14); 
+        var contactMap = new L.Map('map', {maxBounds: ([[39.8462, -8.6931],[40.2169, -8.2278]])}).setView([39.9856, -8.4681], 14); 
 
         L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(contactMap);
 
-        L.marker([40.115, -8.5122]).addTo(contactMap)
+        L.marker([39.9856, -8.4681]).addTo(contactMap)
         .bindPopup(
           '<span style="font-size: small" class="fa featured fa-home"></span>' +
-          '<p>Urb. Qta. do Barroso, Lt. B17 2ºDto</p>' +
-          '<p>3150-292 Sebal</p>' +
+          '<p>Rua da Capela, Serra</p>' +
+          '<p>3240-416 Alvorge</p>' +
           '<p>PORTUGAL</p>' +
           '<span style="font-size: small" class="fa featured fa-mobile"></span>' +
-          '<p>+351 918336529'
+          '<p>+351 918336529' +
+          '<span style="font-size: small" class="fa featured fa-envelope"></span>' +
+          '<a href="mailto:info@geocrafter.eu?Subject=Pedido de contacto" target="_top">info@geocrafter.eu</a>'
           )        
     });
